6a7fc4cd1a90c56c6899f3f5b74ad6ba2e8098f5
Matteo Caberlotto <mcaber@gmail.com>
2017-07-09 17:07:49 +0200

    debug and comments for basic test

--- test1/test.s
+++ test1/test.s
@@ -1,16 +1,16 @@
 .global _start
 
 _start:
-  MOV R0, #0
-  MOV R1, #1
-  B _continue_loop
+  MOV R0, #0 @ load R0 with value 0
+  MOV R1, #1 @ load R1 with value 1
+  B _continue_loop @ branch to _continue_loop
 
 _loop:
-  ADD R0, R0, R1
+  ADD R0, R0, R1 @ add R1 (+1) to R0
 
 _continue_loop:
-  CMP R0, #9
-  BLE _loop
+  CMP R0, #9 @ compare R0 (initially 0) with 9
+  BLE _loop @ branch to _loop if less or equal flag is active
 
 end:
   MOV R7, #1
