bdeee426ead4d7aff0291da201dff1ef1784c745
Matteo Caberlotto <mcaber@gmail.com>
2017-07-09 15:22:25 +0200

    conditional compile

--- make-all.sh
+++ make-all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -e 'ARM_ENV' ]; then
+  echo "Error: environment file ARM_ENV not found. This environment is not meant for compiling"
+  exit 1
+fi
+
 for d in */ ; do
     echo "$d"
     cd "$d"
